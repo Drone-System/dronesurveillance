73bfe83818aba36f5e8bbd2c722ada3ba0f094f0
Casper <casperbramm@hotmail.com>
2025-11-05 11:23:02 +0100

    DRONE-18/CaBr: webserver for cloud

---
+++ cloud/postgres/init.sql
@@ -0,0 +1,42 @@
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+create table if not exists basestations (id serial primary key, name text, ip text);
+create table if not exists users (id serial primary key, username text, password_salt uuid, password text, basestations Integer[]);
+CREATE OR REPLACE FUNCTION public.verify_user(in_username text, in_password text)          
+  RETURNS boolean                                                                           
+  LANGUAGE plpgsql                                                                          
+ AS $function$                                                                              
+ DECLARE                                                                                    
+     stored_hash TEXT;                                                                      
+     stored_salt UUID;                                                                      
+ BEGIN                                                                                      
+     SELECT password, password_salt                                                         
+     INTO stored_hash, stored_salt                                                          
+     FROM users                                                                             
+     WHERE username = in_username;                                                          
+                                                                                            
+     IF NOT FOUND THEN                                                                      
+         RETURN FALSE;                                                                      
+     END IF;                                                                                
+                                                                                            
+     RETURN encode(digest(in_password || stored_salt::text, 'sha256'), 'hex') = stored_hash;
+ END;                                                                                       
+ $function$;
+
+ CREATE OR REPLACE PROCEDURE public.create_user(IN in_username text, IN in_password text, IN in_basestations integer[])
+  LANGUAGE plpgsql                                                                                                     
+ AS $procedure$                                                                                                        
+ DECLARE                                                                                                               
+     salt UUID := gen_random_uuid();                                                                                   
+ BEGIN                                                                                                                 
+     INSERT INTO users (username, password_salt, password, basestations)                                               
+     VALUES (                                                                                                          
+         in_username,                                                                                                  
+         salt,                                                                                                         
+         encode(digest(in_password || salt::text, 'sha256'), 'hex'),                                                   
+         in_basestations                                                                                               
+     );                                                                                                                
+ END;                                                                                                                  
+ $procedure$;
+-- data:
+-- insert into basestations (name, ip) VALUES ('localhost', '127.0.0.1'),('based station', '69.420.69.420');
+-- call create_user('casper', 'stdpw1234', '{1,2}');
