b3f5817d15f810ed3073efa82bd4580da516699d
Vasco Afonso <vafons25@student.aau.dk>
2025-11-10 15:55:37 +0100

    DRONE-16/VaAf: First draft of base station core video dispatcher

---
+++ base_station/core/tests/DummyConsumer.py
@@ -0,0 +1,49 @@
+from flask import Flask, Response
+from kafka import KafkaConsumer
+import sys
+
+
+topic = "0_0"
+consumer = KafkaConsumer(topic, bootstrap_servers=['localhost:9092'])
+consumer2 = KafkaConsumer("0_1", bootstrap_servers=['localhost:9092'])
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    cons = KafkaConsumer(bootstrap_servers=['localhost:9092'])
+    print(cons.topics())
+    a = ""
+    for topic in cons.topics():
+        a += topic
+    return a
+
+@app.route('/kaf/', methods=['GET'])
+def video():
+    return Response(kafks(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    # print("Request")
+    # return kafks()
+
+def kafks():
+    for message in consumer:
+        print("1- ", len(message))
+        yield (b'--frame\r\n'
+        b'Content-Type: image/jpg\r\n\r\n' + message.value + b'\r\n\r\n')
+        # # print(message.value)
+        # return message.value
+
+@app.route('/kaf2/', methods=['GET'])
+def video2():
+    return Response(kafks1(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    # print("Request")
+    # return kafks()
+
+def kafks1():
+    for message in consumer2:
+        print("2", len(message))
+        yield (b'--frame\r\n'
+        b'Content-Type: image/jpg\r\n\r\n' + message.value + b'\r\n\r\n')
+        # # print(message.value)
+        # return message.value
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
