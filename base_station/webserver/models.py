ecae6fd11bcfb95217c0ff388c735e0027fdb527
Sebastian Anderson <sebastianderson@MacBook-Pro-de-Sebastian.local>
2025-12-04 21:56:02 +0100

    Added new features + database

--- base_station/webserver/models.py
+++ base_station/webserver/models.py
@@ -1,5 +1,7 @@
+from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-from config import db
+from config import app, db
+
 
 class Protocol(db.Model):
     __tablename__ = 'protocols'
@@ -9,31 +11,42 @@ class Protocol(db.Model):
     def __repr__(self):
         return f"<Protocol {self.name}>"
 
+
 class Device(db.Model):
     __tablename__ = 'devices'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120))
+
     ip = db.Column(db.String(45), unique=True, nullable=False)
     port = db.Column(db.Integer, default=80)
+
+    username = db.Column(db.String(100))
+    password = db.Column(db.String(100))
+
     added_at = db.Column(db.DateTime, default=datetime.utcnow)
+
     protocol_id = db.Column(db.Integer, db.ForeignKey('protocols.id'))
     protocol = db.relationship('Protocol')
 
     type = db.Column(db.String(50))
-    __mapper_args__ = {'polymorphic_on': type, 'polymorphic_identity': 'device'}
+    __mapper_args__ = {"polymorphic_on": type, "polymorphic_identity": "device"}
+
 
 class Drone(Device):
     __tablename__ = 'drones'
     id = db.Column(db.Integer, db.ForeignKey('devices.id'), primary_key=True)
     model = db.Column(db.String(100))
-    status = db.Column(db.String(50), default='idle')
+    status = db.Column(db.String(50), default="idle")
 
-    __mapper_args__ = {'polymorphic_identity': 'drone'}
+    __mapper_args__ = {"polymorphic_identity": "drone"}
+
 
 class Camera(Device):
     __tablename__ = 'cameras'
     id = db.Column(db.Integer, db.ForeignKey('devices.id'), primary_key=True)
+
+    camera_type = db.Column(db.String(100))  # NEW FIELD
     resolution = db.Column(db.String(50))
     fps = db.Column(db.Integer)
 
-    __mapper_args__ = {'polymorphic_identity': 'camera'}
+    __mapper_args__ = {"polymorphic_identity": "camera"}
