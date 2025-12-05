587ff1ecfb7d03d007634632b53f194d9eaa8e90
Vasco Afonso <vafons25@student.aau.dk>
2025-12-05 19:54:48 +0100

    DRONE-16/VaAf: Start integration of grpc server

---
+++ cloud/proxy/DroneWebRtc.py
@@ -0,0 +1,51 @@
+import ServerBaseStation_pb2, ServerBaseStation_pb2_grpc
+from channel import Channel
+
+class DroneWebRtc(ServerBaseStation_pb2_grpc.DroneWebRtcServicer):
+    def __init__(self, communication: dict):
+        self.communication = communication
+
+    async def Connect(
+        self,
+        request: ServerBaseStation_pb2.ConnectRequest,
+        context: grpc.aio.ServicerContext,
+    ) -> ServerBaseStation_pb2.ConnectResponse:
+        basestation_id = request.basestation_id
+        stream_id = str(uuid.uuid4())
+        self.communication.update({basestation_id: {stream_id:Channel(request.name)}})
+        print('connect', self.communication)
+        return ServerBaseStation_pb2.ConnectResponse(stream_id=stream_id)
+    
+    async def Stream(
+        self,
+        request: ServerBaseStation_pb2.StreamOffer,
+        context: grpc.aio.ServicerContext,
+    ) -> ServerBaseStation_pb2.StreamAnswer:
+        # offer = {'stream_id':request.stream_id,'offer':{'type':request.offer.type, 'sdp':request.offer.sdp}}
+        comm = self.communication[request.basestation_id][request.stream_id]
+        comm.offer = request
+        while not comm.answer:
+            await asyncio.sleep(0.5)
+
+        print(comm.answer)
+        # answer = ServerBaseStation_pb2.StreamAnswer(stream_id=comm.answer['stream_id'], 
+        #                               answer=ServerBaseStation_pb2.StreamDesc(
+        #                                   type=comm.answer['answer']['type'],
+        #                                   sdp=comm.answer['answer']['sdp']),
+        #                               )
+        answer = comm.answer
+        comm.answer = None
+        print("ANNN", answer)
+        return answer
+    
+    async def Poll(self,
+        request: ServerBaseStation_pb2.PollRequest,
+        context: grpc.aio.ServicerContext,
+    ) -> ServerBaseStation_pb2.PollResponse:
+        basestation_id = request.basestation_id
+        stream_id = request.stream_id
+        requested = False
+        if self.communication[basestation_id][stream_id].requested:
+            requested = True
+            
+        return ServerBaseStation_pb2.PollResponse(stream_needed=requested)
---
+++ cloud/proxy/WebRtcReceiver.py
@@ -0,0 +1,115 @@
+from VideoReceiver import VideoReceiver
+import ServerBaseStation_pb2, ServerBaseStation_pb2_grpc
+
+class WebRTCReceiver(ServerBaseStation_pb2_grpc.WebRtcServicer):
+    def __init__(self):
+        self.tracks = {}
+        self.producers = {}  # producer_sid -> {stream_id, name, pc}
+        self.peer_connections = {}  # producer_sid -> RTCPeerConnection
+        self.video_receivers = {}
+
+
+    async def Connect(
+        self,
+        request: ServerBaseStation_pb2.ConnectRequest,
+        context: grpc.aio.ServicerContext,
+    ) -> ServerBaseStation_pb2.ConnectResponse:
+        print("Connected")
+        return ServerBaseStation_pb2.ConnectResponse(stream_id=str(uuid.uuid4()))
+
+    async def Register(
+        self,
+        request: ServerBaseStation_pb2.RegisterProducerRequest,
+        context: grpc.aio.ServicerContext,
+    ) -> ServerBaseStation_pb2.RegisterProducerResponse:
+        print("Registered producer:", request.name)
+        stream_name = request.name
+        # stream_id = request.sid
+        self.producers[request.stream_id] = {
+            'stream_id': request.stream_id,
+            'name': stream_name,
+        }
+        return ServerBaseStation_pb2.RegisterProducerResponse(stream_id=request.stream_id, viewer_sid='server')
+
+    async def Stream(
+        self,
+        request: ServerBaseStation_pb2.StreamOffer,
+        context: grpc.aio.ServicerContext,
+    ) -> ServerBaseStation_pb2.StreamAnswer:
+        print("Stream Offer Received")
+        stream_name = self.producers.get(request.stream_id, {}).get('name', 'Unknown')
+        stream_id = self.producers.get(request.stream_id, {}).get('stream_id', request.stream_id)
+        
+        print(f"Received offer from producer: {stream_name}")
+        offer = request.offer
+        
+        # Create peer connection
+        pc = RTCPeerConnection()
+        self.peer_connections[request.stream_id] = pc
+        
+        @pc.on("track")
+        async def on_track(track):
+            print(f"Received track: {track.kind} from {stream_name}", track.id, request.stream_id)
+            
+            if track.kind == "video":
+
+
+                mrec = MediaRecorder(f"{track.id}.mp4", options={"framerate": "30", "video_size": "1080"})
+                relay = MediaRelay()
+
+                mrec.addTrack(relay.subscribe(track))
+                video_receiver= VideoReceiver(request.stream_id,  self.producers[request.stream_id]['name']) 
+                signal = asyncio.Event()
+                asyncio.ensure_future(video_receiver.handle_track(relay.subscribe(track), request.stream_id, signal))
+                await mrec.start()
+
+                try:
+                    self.video_receivers[request.stream_id] = (video_receiver, signal)
+
+                except Exception as e:
+                    print(f"Track ended for {stream_name}: {e}")
+        @pc.on("connectionstatechange")
+        async def on_connection_changed():
+            print("state: ", pc.connectionState)
+
+        # # Handle ICE candidates
+        @pc.on("icecandidate")
+        async def on_icecandidate(candidate):
+            print("received ice")
+            if candidate:
+                print("received", candidate)
+        
+        # Set remote description
+        await pc.setRemoteDescription(
+            RTCSessionDescription(sdp=offer.sdp, type=offer.type)
+        )
+        
+        # Create and send answer
+        answer = await pc.createAnswer()
+        await pc.setLocalDescription(answer)
+        
+
+        print(pc.localDescription.type)
+        print(pc.localDescription.sdp)
+        return ServerBaseStation_pb2.StreamAnswer(
+            stream_id=stream_id, #'server', 
+            answer=ServerBaseStation_pb2.StreamDesc(
+                type=pc.localDescription.type, 
+                sdp=pc.localDescription.sdp
+                ))
+    
+    async def Disconnect(
+        self,
+        request: ServerBaseStation_pb2.DisconnectRequest,
+        context: grpc.aio.ServicerContext,
+    ) -> ServerBaseStation_pb2.DisconnectResponse:
+        print("Received disc")
+        print(self.video_receivers)
+        try:
+            self.video_receivers[request.stream_id][1].set()
+            self.video_receivers.pop(request.stream_id) 
+        except:
+            print("what the flip")
+        if self.peer_connections[request.stream_id].connectionState != "closed":
+            await self.peer_connections[request.stream_id].close()
+        return ServerBaseStation_pb2.DisconnectResponse()
