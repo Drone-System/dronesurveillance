aebe1cf59207c11a056b70ee35573a3025ddbcd8
Vasco Afonso <vafons25@student.aau.dk>
2025-12-06 16:27:39 +0100

    DRONE-16/VaAf: Added permission verification to cloud webserver and removed groups from db

--- cloud/postgres/init.sql
+++ cloud/postgres/init.sql
@@ -1,9 +1,9 @@
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 create table if not exists basestations (id serial primary key, name text unique, password_salt uuid, password text);
 create table if not exists users (id serial primary key, username text unique, password_salt uuid, password text);
-create table if not exists groups (id serial primary key, name text unique);
-create table if not exists users_to_groups (user_id integer references users(id) on delete cascade, group_id integer references groups(id) on delete cascade, owner boolean);
-create table if not exists basestations_to_groups (basestation_id integer references basestations(id) on delete cascade, group_id integer references groups(id) on delete cascade);
+-- create table if not exists groups (id serial primary key, name text unique);
+create table if not exists users_to_basestations (user_id integer references users(id) on delete cascade, basestation_id integer references basestations(id) on delete cascade, owner boolean);
+-- create table if not exists basestations_to_groups (basestation_id integer references basestations(id) on delete cascade, group_id integer references groups(id) on delete cascade);
 CREATE OR REPLACE FUNCTION public.verify_user(in_username text, in_password text)          
   RETURNS boolean                                                                           
   LANGUAGE plpgsql                                                                          
@@ -25,7 +25,7 @@ CREATE OR REPLACE FUNCTION public.verify_user(in_username text, in_password text
  END;                                                                                       
  $function$;
 
- CREATE OR REPLACE FUNCTION public.verify_basestation(in_basestation_name text, in_password text)          
+ CREATE OR REPLACE FUNCTION public.verify_basestation(in_basestation_id integer, in_password text)          
   RETURNS boolean                                                                           
   LANGUAGE plpgsql                                                                          
  AS $function$                                                                              
@@ -36,7 +36,7 @@ CREATE OR REPLACE FUNCTION public.verify_user(in_username text, in_password text
      SELECT password, password_salt                                                         
      INTO stored_hash, stored_salt                                                          
      FROM basestations                                                                             
-     WHERE name = in_basestation_name;                                                          
+     WHERE id = in_basestation_id;                                                          
                                                                                             
      IF NOT FOUND THEN                                                                      
          RETURN FALSE;                                                                      
@@ -80,6 +80,7 @@ call create_user('casper', 'stdpw1234');
 call create_basestation('basestation_one', 'qwerty123');
 call create_basestation('based station', '420!nice');
 call create_basestation('outsider base station', 'someone else');
-insert into groups(name) VALUES ('the best group');
-insert into users_to_groups(user_id, group_id, owner) VALUES (1, 1, TRUE);
-insert into basestations_to_groups(basestation_id, group_id) VALUES (1, 1),(2, 1);
+insert into users_to_basestations(user_id, basestation_id, owner) VALUES (1, 1, TRUE);
+-- insert into groups(name) VALUES ('the best group');
+-- insert into users_to_groups(user_id, group_id, owner) VALUES (1, 1, TRUE);
+-- insert into basestations_to_groups(basestation_id, group_id) VALUES (1, 1),(2, 1);
