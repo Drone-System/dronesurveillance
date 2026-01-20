166b9d317bf37a9994f8099de28e3cb91764b313
Vasco Afonso <vafons25@student.aau.dk>
2026-01-20 12:42:58 +0100

    DRONE-XX/VaAf: Clean-up

--- base_station/core/alt_main.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-from drone.WebRtcDrone import DroneWebRTCProducer
-from drone.DjiTello import DjiTelloDrone
-async def main():
-    p = DroneWebRTCProducer(1, DjiTelloDrone, 'stream')
-    await p.start(asyncio.Event())
-
-if __name__ == "__main__":
-    asyncio.run(main())
